e77760d3ef4e12bc64bf36449004d4613c12f82a
sml87 <szewczyk.home@gmail.com>
2022-06-27 21:28:58 +0200

    refactoring code

--- backend/utils/authToken.ts
+++ backend/utils/authToken.ts
@@ -1,25 +1,25 @@
 import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 
-const secret = '2b328988-28b7-40b1-ba7f-90aa6c678e0e';
+const secret = process.env.JWT_SECRET;
 
 export const authToken = (id: string): string => {
   return jwt.sign({ id }, secret, {
-    expiresIn: '10min',
+    expiresIn: '60min',
   });
 };
 
 export const authorization = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.access_token;
   if (!token) {
-    return res.sendStatus(403);
+    return res.status(403);
   }
   try {
     const data = jwt.verify(token, secret);
-    // @todo create type
-
+    // @ts-ignore
+    req.userId = data.id;
     return next();
   } catch {
-    return res.sendStatus(403);
+    return res.status(403);
   }
 };
